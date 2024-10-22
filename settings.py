75d899fa0293edfedde5495cda873ae4d0805878
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-10-22 12:37:26 +0300

    static

--- settings.py
+++ settings.py
@@ -86,7 +86,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATIC_URL = 'static'
 
 # Additional locations of static files
 STATICFILES_DIRS = [
