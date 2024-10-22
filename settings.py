8788a0d743efcb27866f6060e35188e67a537c4a
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-10-22 08:52:02 +0300

    settings-up-to-date

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 # settings.py
 
+
 import os
 import sys
 from pathlib import Path
@@ -74,11 +75,11 @@ if DEBUG:
 else:
     DATABASES = {
         'default': dj_database_url.config(
-            default=os.getenv('DATABASE_URL', '')
+            default=os.getenv('DATABASE_URL')
         )
     }
 
-    # Raise an error if DATABASE_URL is not set correctly
+    # Verifica si DATABASE_URL está configurado correctamente
     if not DATABASES['default']:
         raise ValueError("No DATABASE_URL environment variable set. Please set it in Railway.")
 
