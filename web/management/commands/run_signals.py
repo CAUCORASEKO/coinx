52061c6d0d5ebf4bee0e9653dd4be26a3ad6d346
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-10-23 14:30:49 +0300

    instructiones

--- web/management/commands/run_signals.py
+++ web/management/commands/run_signals.py
@@ -5,17 +5,17 @@ import threading
 import sys
 import os
 
-# Añadimos la raíz del proyecto al sys.path para poder importar el archivo de señales
+# Adding the project root to sys.path to import the signal file
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
-#from trade_signal import perform_trade_analysis  
-# Import primary function to script
+# from trade_signal import perform_trade_analysis  
+# Import the main function for the script
 
 class Command(BaseCommand):
-    help = 'Ejecuta el script de señales de trading'
+    help = 'Runs the trading signals script'
 
     def handle(self, *args, **kwargs):
-        # Inicia el script de señales en un hilo separado
-       # thread = threading.Thread(target=perform_trade_analysis)
-       # thread.start()
-        self.stdout.write(self.style.SUCCESS(' Script start.'))
+        # Start the signal script in a separate thread
+        # thread = threading.Thread(target=perform_trade_analysis)
+        # thread.start()
+        self.stdout.write(self.style.SUCCESS('Script started.'))
