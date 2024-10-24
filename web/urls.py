19f7c738b6f0893c75dd669d3aaca83b1efa00cd
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-10-24 12:17:24 +0300

    web/url.py

--- web/urls.py
+++ web/urls.py
@@ -1,26 +1,26 @@
-# urls
+# web/urls.py 
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from web import views
 
 urlpatterns = [
-    # Home and registration
+    # Koti- ja rekisteröintisivut
     path('', views.index, name='index'),
     path('register/', views.register_view, name='register'),
     path('confirm/<str:token>/', views.confirm_email, name='confirm_email'),
 
-    # Login and logout
+    # Kirjautumis- ja uloskirjautumissivut
     path('accounts/login/', auth_views.LoginView.as_view(template_name='web/registration/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
 
-    # Dashboard
+    # Käyttäjän hallintapaneeli
     path('dashboard/', views.dashboard_view, name='dashboard'),
-    #path('get_signal/', views.get_signal, name='get_signal'),
+    #path('get_signal/', views.get_signal, name='get_signal'),  # Poistettu käytöstä, jos ei tarvita
 
-    # Update API keys
+    # API-avainten päivityssivu
     path('update-api-keys/', views.update_api_keys, name='update_api_keys'),
 
-    # Password reset URLs
+    # Salasanan nollaus-URL:t
     path('password-reset/', 
          auth_views.PasswordResetView.as_view(template_name='web/password_reset.html'), 
          name='password_reset'),
@@ -34,12 +34,10 @@ urlpatterns = [
          auth_views.PasswordResetCompleteView.as_view(template_name='web/password_reset_complete.html'), 
          name='password_reset_complete'),
 
-    # Username recovery URLs
+    # Käyttäjätunnuksen palautus-URL
     path('username-recovery/', views.username_recovery_view, name='username_recovery'),
 
-   
-    # Payment URLs
+    # Maksusivujen URL:t
     path('payment/', views.payment_subscription, name='payment_subscription'),
-    path('payment/instructions/<str:plan>/', views.payment_instructions, name='payment_instructions'),  # Cambiar 'instructions' a 'payment_instructions'
-
+    path('payment/instructions/<str:plan>/', views.payment_instructions, name='payment_instructions'),  # Maksuohjeiden URL
 ]
