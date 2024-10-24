d417d57b8ca76e70a4da87de98ec16fb50e42667
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-10-24 11:38:27 +0300

    models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,4 @@
+# models.py
 from django.db import models
 from django.contrib.auth.models import User
 from cryptography.fernet import Fernet
@@ -7,7 +8,7 @@ import uuid
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    # Campos adicionales
+    # Lisäkentät käyttäjäprofiilille
     real_name = models.CharField(max_length=100, null=True)
     last_name = models.CharField(max_length=100, null=True)
     country = models.CharField(max_length=100, null=True)
@@ -15,19 +16,19 @@ class UserProfile(models.Model):
     postal_code = models.CharField(max_length=20, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
 
-    # Claves API y plataforma
+    # API-avaimet ja alusta
     platform = models.CharField(max_length=100, default='binance')
     api_key_encrypted = models.BinaryField(default=b'')
     api_secret_encrypted = models.BinaryField(default=b'')
     email_confirmation_token = models.UUIDField(default=uuid.uuid4)
 
-    # Sobrescribir el método save() para cifrar las claves API
+    # save()-metodin ylikirjoitus API-avainten salaamiseksi
     def save(self, *args, **kwargs):
-        # Verificamos si las claves API están presentes
+        # Tarkistetaan, ovatko API-avaimet olemassa
         if self.api_key_encrypted and self.api_secret_encrypted:
             fernet = Fernet(settings.ENCRYPTION_KEY)
             
-            # Cifrar solo si se pasaron nuevas claves API en los kwargs
+            # Salaa vain, jos uusia API-avaimia annetaan kwargs:ssä
             if 'api_key' in kwargs:
                 self.api_key_encrypted = fernet.encrypt(kwargs['api_key'].encode())
             if 'api_secret' in kwargs:
@@ -35,14 +36,14 @@ class UserProfile(models.Model):
 
         super().save(*args, **kwargs)
 
-    # Obtener la clave API descifrada
+    # Palautetaan salattu API-avain
     def get_api_key(self):
         fernet = Fernet(settings.ENCRYPTION_KEY)
         if self.api_key_encrypted:
             return fernet.decrypt(self.api_key_encrypted).decode()
         return None
 
-    # Obtener el API Secret descifrado
+    # Palautetaan salattu API-salaisuus
     def get_api_secret(self):
         fernet = Fernet(settings.ENCRYPTION_KEY)
         if self.api_secret_encrypted:
@@ -53,7 +54,7 @@ class UserProfile(models.Model):
         return self.user.username
 
 
-# Nueva seccion de pagos
+# Maksutapahtumien malli
 
 class PaymentTransaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -69,7 +70,7 @@ class PaymentTransaction(models.Model):
         return f"Transaction {self.transaction_id} for user {self.user.username}"
 
 
-# Payments user details registration
+# Maksujen rekisteröinti käyttäjätietoineen
 
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payments')
