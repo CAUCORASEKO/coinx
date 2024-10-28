99d500aadee96ec4a5ab5c0851bc4e29af6b7a19
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-10-28 11:14:11 +0200

    email_and_password

--- settings.py
+++ settings.py
@@ -106,8 +106,8 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    EMAIL_HOST_USER = os.getenv('latribubooking@gmail.com')
+    EMAIL_HOST_PASSWORD = os.getenv('Raseko2024')
     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # CoinPayments API-avaimet
